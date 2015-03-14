7f2bfa9f99ec414259db4ac1e8786851790c8f39
Jiming Ye <415386365@qq.com>
2015-03-15 02:14:47 +0800

    Create ftrl.py

---
+++ python/ftrl.py
@@ -0,0 +1,107 @@
+from csv import DictReader
+import csv
+from math import exp, log, sqrt
+
+alpha = .05  # learning rate
+beta = 1.   # smoothing parameter for adaptive learning rate
+L1 = 1.1     # L1 regularization, larger value means more regularized
+L2 = 1.1     # L2 regularization, larger value means more regularized
+
+D = 2 ** 24           # number of weights to use
+interaction = False     # whether to enable poly2 feature interactions
+
+class ftrl_proximal(object):
+
+    def __init__(self, alpha, beta, L1, L2, D, interaction):
+        # parameters
+        self.alpha = alpha
+        self.beta = beta
+        self.L1 = L1
+        self.L2 = L2
+
+        # feature related parameters
+        self.D = D
+        self.interaction = interaction
+
+        # model
+        # n: squared sum of past gradients
+        # z: weights
+        # w: lazy weights
+        self.n = [0.] * D
+        self.z = [0.] * D
+        self.w = {}
+
+    def _indices(self, x):
+
+        # first yield index of the bias term
+        yield 0
+
+        # then yield the normal indices
+        for index in x:
+            yield index
+
+        # now yield interactions (if applicable)
+        if self.interaction:
+            D = self.D
+            L = len(x)
+
+            x = sorted(x)
+            for i in xrange(L):
+                for j in xrange(i+1, L):
+                    # one-hot encode interactions with hash trick
+                    yield int(hash(str(x[i]) + '_' + str(x[j]))) % D
+
+    def predict(self, x):
+
+        # parameters
+        alpha = self.alpha
+        beta = self.beta
+        L1 = self.L1
+        L2 = self.L2
+
+        # model
+        n = self.n
+        z = self.z
+        w = {}
+
+        # wTx is the inner product of w and x
+        wTx = 0.
+        for i in self._indices(x):
+            sign = -1. if z[i] < 0 else 1.  # get sign of z[i]
+
+            # build w on the fly using z and n, hence the name - lazy weights
+            # we are doing this at prediction instead of update time is because
+            # this allows us for not storing the complete w
+            if sign * z[i] <= L1:
+                # w[i] vanishes due to L1 regularization
+                w[i] = 0.
+            else:
+                # apply prediction time L1, L2 regularization to z and get w
+                w[i] = (sign * L1 - z[i]) / ((beta + sqrt(n[i])) / alpha + L2)
+
+            wTx += w[i]
+
+        # cache the current w for update stage
+        self.w = w
+
+        # bounded sigmoid function, this is the probability estimation
+        return 1. / (1. + exp(-max(min(wTx, 35.), -35.)))
+
+    def update(self, x, p, y):
+
+        # parameter
+        alpha = self.alpha
+
+        # model
+        n = self.n
+        z = self.z
+        w = self.w
+
+        # gradient under logloss
+        g = p - y
+
+        # update z and n
+        for i in self._indices(x):
+            sigma = (sqrt(n[i] + g * g) - sqrt(n[i])) / alpha
+            z[i] += g - sigma * w[i]
+            n[i] += g * g
