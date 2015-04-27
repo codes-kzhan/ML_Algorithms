896454afb91756b9eb6a96b11232c84f0ff3ba81
Jiming Ye <415386365@qq.com>
2015-04-28 00:22:33 +0800

    Update utils.pyx

--- python/OnlineRandomForest/utils.pyx
+++ python/OnlineRandomForest/utils.pyx
@@ -1,66 +1,68 @@
 import numpy as np
 cimport numpy as np
-from libc.math import sqrt, abs
+cimport cython
+from libc.math cimport sqrt, abs
 
-cdef str argmax(dict d) except *:
-	cdef str max_class = None
-	cdef double max_count = 0
-	cdef double total_count = 0
-	cdef str key
-	cdef double value
-	for key, value in d.iteritems():
-		total_count += value
-		if value > max_count:
-			max_count = value
-			max_class = key
-	
-	return max_class
+ctypedef np.int_t DTYPE_t
+
+cpdef DTYPE_t argmax(dict d):
+    cdef double max_count = 0
+    cdef double total_count = 0
+    cdef double value
+    cdef DTYPE_t key
+    cdef DTYPE_t max_class = 0
+    for key, value in d.iteritems():
+        total_count += value
+        if value > max_count:
+            max_count = value
+            max_class = key
+    return max_class
 
 
-cdef str predict_max(list a) except *:
-	return argmax(count_dict(a))
+def predict_max(list a):
+    return argmax(count_dict(a))
 
-cdef dict count_dict(list a) except *:
-	cdef dict d = {}
-	cdef str x
-	for x in a:
-		d.setdefault(x, 0)
-		d[x] += 1
-	return d
+cpdef dict count_dict(list a):
+    cdef DTYPE_t x
+    cdef dict d = {}
+    for x in a:
+        d.setdefault(x, 0)
+        d[x] += 1
+    return d
 
-cdef double mean_squared_error(list x):
-	cdef np.array xnp
-	xnp = np.array(x)
-	xnp = xnp - xnp.mean()
-	return sqrt((xnp * xnp.T).mean())
+cpdef double mean_squared_error(list x):
+    cdef np.ndarray xnp
+    xnp = np.array(x)
+    xnp = xnp - xnp.mean()
+    return sqrt((xnp * xnp.T).mean())
 
-cdef double mean_absolute_error(list x):
-	cdef np.array xnp
-	xnp = np.array(x)
-	xnp = xnp - xnp.mean()
-	return abs(xnp).mean()
+cpdef double mean_absolute_error(list x):
+    cdef np.ndarray xnp
+    xnp = np.array(x)
+    xnp = xnp - xnp.mean()
+    return abs(xnp).mean()
 
-cdef double gini(list x):
-	cdef dict d = {}
-	cdef str y
-	cdef double total
-	cdef list to_square
-	cdef np.ndarray to_square2
-	for y in x:
-		d.setdefault(y, 0)
-		d[y] += 1
-	total = len(x)
-	to_square = []
-	cdef str key
-	cdef double value
-	for key, value in d.iteritems():
-		to_square.append(value/total)
-	to_square2 = np.array(to_square)
-	return 1 - (to_square2 * to_square.T).sum()
+cpdef double gini(list x):
+    cdef dict d = {}
+    cdef double total
+    cdef list to_square
+    cdef np.ndarray to_square2
+    cdef DTYPE_t y
+    for y in x:
+        d.setdefault(y, 0)
+        d[y] += 1
+    total = len(x)
+    to_square = []
+    cdef double value
+    cdef DTYPE_t key
+    for key, value in d.iteritems():
+        to_square.append(value/total)
+    to_square2 = np.array(to_square)
+    return 1 - (to_square2 * to_square2.T).sum()
 
-cdef tuple bin_split(list sample_feature, double feature_value):
-	cdef np.ndarray left, right
-	cdef tuple x
-	left = np.array([x[1] for x in sample_feature if x[0]<=feature_value])
-	right = np.array([x[1] for x in sample_feature if x[0]>feature_value])
-	return left, right
+cpdef tuple bin_split(list sample_feature, double feature_value):
+    cdef list left, right
+    cdef tuple x
+    left = [x[1] for x in sample_feature if x[0]<=feature_value]
+    right = [x[1] for x in sample_feature if x[0]>feature_value]
+    return left, right
