84a08736c07c529fbbe9c05000ee26bc322e54e3
Jiming Ye <415386365@qq.com>
2015-05-12 22:41:17 +0800

    Update LogisticRegression.scala

--- spark/LogisticRegression.scala
+++ spark/LogisticRegression.scala
@@ -1,47 +1,67 @@
+import java.util.Random
+
+import scala.math.exp
+
+import breeze.linalg.{Vector, DenseVector}
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionModel}
-import org.apache.spark.mllib.evaluation.MulticlassMetrics
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.linalg.Vectors
-import scala.collection.mutable.ListBuffer
 
-object logistic {
+
+object Logistic {
+  val iterations = 1
+  val rand = new Random(42)
+  
+  case class DataPoint(x: Vector[Double], y: Double)
+  
+  def parsePoint(line: String): DataPoint = {
+    var values = line.split(',')
+    values(93) = values(93).last.toString()
+    var values2 = values.map(_.toDouble)
+    values2(93) = values2(93) - 1
+    val x = Vector(values2.init)
+    val y = {
+      if (values2(93) == 0) -1
+      else 1
+    }
+    DataPoint(x, y)
+  }
+  
+  def score(p: DataPoint, w: DenseVector[Double]): Int = {
+    val proba = 1 / (1 + exp(-w.dot(p.x)))
+    val predict: Double = {
+      if (proba > 0.5) 1.0
+      else -1.0
+    }
+    if (predict == p.y) 1
+    else 0
+  }
+  
+  def main(args: Array[String]) {
     
-    def parsePoint(line: String): LabeledPoint = {
-        var values = line.split(',')
-        values(93) = values(93).last.toString()
-        var values2 = values.map(_.toDouble)
-        values2(93) = values2(93) - 1
-        LabeledPoint(values2.last, Vectors.dense(values2.init))
+    val conf = new SparkConf().setAppName("Logistic Regression")
+    val sc = new SparkContext(conf)
+    val csv_file = sc.textFile("/home/yejiming/desktop/spark/scala/dataset.csv")
+    val data = csv_file.map(parsePoint)
+    
+    var w = DenseVector.fill(93){2 * rand.nextDouble - 1}
+    println("Initial w: " + w)
+    
+    for (i <- 1 to iterations) {
+      println("On iteration " + i)
+      val gradient = data.map { p =>
+        p.x * (1 / (1 + exp(-p.y * (w.dot(p.x)))) - 1) * p.y
+      }.reduce((a, b) => a + b)
+      w -= gradient
     }
-
-    def main(args: Array[String]) {
-        val conf = new SparkConf().setAppName("logistic regression")
-        val sc = new SparkContext(conf)
-
-        // Load training data in csv format.
-        val csv_file = sc.textFile("/home/yejiming/desktop/spark/scala/dataset.csv")
-        val data = csv_file.map(parsePoint)
-
-        // Split data into training (80%) and test (20%).
-        val splits = data.randomSplit(Array(0.8, 0.2), seed = 123)
-        val training = splits(0).cache()
-        val test = splits(1)
-
-        // Run training algorithm to build the model
-        val model = new LogisticRegressionWithLBFGS().setNumClasses(9).run(training)
-
-        // Compute raw scores on the test set.
-        val predictionAndLabels = test.map { 
-            case LabeledPoint(label, features) =>
-            val prediction = model.predict(features)
-            (prediction, label)
-        }
-
-        // Get evaluation metrics.
-        val metrics = new MulticlassMetrics(predictionAndLabels)
-        val precision = metrics.precision
-        println("Precision = " + precision)
-    }
+    
+    println("Final w: " + w)
+    val correct_num: Double = data.map(p => score(p, w)).reduce(_ + _)
+    val s: Double = correct_num / data.count()
+    println("Prediction accuracy: " + s)
+    
+    sc.stop()
+  }
 }
